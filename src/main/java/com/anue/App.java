7c8b5c18c34dc32efd08a76016872e8b5ac277ee
Edward Lin <edward.lin@positivegrid.com>
2019-02-27 00:01:51 +0800

    fix

--- src/main/java/com/anue/App.java
+++ src/main/java/com/anue/App.java
@@ -13,7 +13,7 @@ public class App
 {
     public static void main( String[] args )
     {
-    	Security.addProvider(new BouncyCastleProvider());
+    	Security.addProvider(new BouncyCastlePQCProvider());
     	if (0 == args.length)
         System.out.println("Hello World!");
     	else {
